6f8787a3880bf6cc9601fd6b3a3e39263ad4f708
isaacsc <isaacsan@gmail.com>
2018-12-07 17:05:12 +0100

    Fetch event exercise

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,14 +1,7 @@
 self.addEventListener('fetch', function(event) {
-    self.addEventListener('fetch', function(event) {
-        // TODO: respond to all requests with an html response
-        // containing an element with class="a-winner-is-me".
-        // Ensure the Content-Type of the response is "text/html"
-      event.respondWith(
-        new Response('<div class="a-winner-is-me"> SW html test!</div>', {
-          headers: {
-            'Content-Type': 'text/html'
-          }
-        })
-      )
-    });
+  // TODO: only respond to requests with a
+  // url ending in ".jpg"
+  event.respondWith(
+    fetch('/imgs/dr-evil.gif')
+  );
 });
