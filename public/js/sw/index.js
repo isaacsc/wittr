88bb8f652336effcf93accedc9f852807b796195
isaacsc <isaacsan@gmail.com>
2018-12-07 16:50:48 +0100

    Respond to all request with an html response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,14 @@
 self.addEventListener('fetch', function(event) {
-  console.log(event.request);
+    self.addEventListener('fetch', function(event) {
+        // TODO: respond to all requests with an html response
+        // containing an element with class="a-winner-is-me".
+        // Ensure the Content-Type of the response is "text/html"
+      event.respondWith(
+        new Response('<div class="a-winner-is-me"> SW html test!</div>', {
+          headers: {
+            'Content-Type': 'text/html'
+          }
+        })
+      )
+    });
 });
